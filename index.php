cce240dc00330eccd9ac2f5fcb799075adc4834c
Ramadan Saputra <rarasta27@gmail.com>
2018-07-20 11:19:42 +0700

    Index.php
    => route chart berdasarkan var get

--- index.php
+++ index.php
@@ -1,2 +1,25 @@
 <?php
-	
+	// inisilisasi var $_GET
+	$get_chart = isset($_GET['chart']) ? $_GET['chart'] : false;
+
+	// cek $_GET
+	if(!$get_chart) die('Pastikan URL Anda Benar');
+	else{
+		switch (strtolower($get_chart)) {
+			case 'chart1':
+				# code...
+				break;
+
+			case 'chart1':
+				# code...
+				break;
+
+			case 'chart1':
+				# code...
+				break;
+			
+			default:
+				# code...
+				break;
+		}
+	}
